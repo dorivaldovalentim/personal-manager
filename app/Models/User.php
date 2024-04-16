529e33736a18aa2a1d044621d17008878c58b397
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2024-04-16 17:54:19 +0100

    Mudei o tipo de dado do id de user de int para string (uuid)

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -17,6 +18,7 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasUuids;
 
     /**
      * The attributes that are mass assignable.
