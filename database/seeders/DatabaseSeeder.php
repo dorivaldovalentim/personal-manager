f687f00f1ad98688a3ff7d12d29c84d18fbfd9a1
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2024-04-19 11:05:32 +0100

    Actualizei o seeder DatabaseSeeder para cadastrar um admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make(env("DEFAULT_PASSWORD"))
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
