b289ac785c65925b454b56719a7923c7dbe0d4c6
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2024-04-20 01:45:36 +0100

    Implementei o método de login

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,9 @@
 <?php
+use App\Http\Controllers\Auth\AdminLoginController;
 
 Route::domain(env("ADMIN_DOMAIN"))->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', function() { return 'admin'; });
+    Route::middleware("guest:admin")->group(function () {
+        Route::inertia("/login", "Admin/Auth/Login")->name("login");
+        Route::post("/login", [AdminLoginController::class, "store"])->name("login");
+    });
 });
